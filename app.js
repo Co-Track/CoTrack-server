9aefa267ef4828761966a4a03c0d0f6a4e7cbbdd
Menatalla Gomaa <minafathi98@gmail.com>
2023-12-05 17:09:47 +0100

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
-
+console.log("daniel was here");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
